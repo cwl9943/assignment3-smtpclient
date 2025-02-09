e9a73683a6f79bb5178b4bb79cd5bc9752ec3538
cwl9943 <cwl9943@nyu.edu>
2025-02-09 17:41:14 -0500

    Update smtpClient.py

--- smtpClient.py
+++ smtpClient.py
@@ -82,6 +82,15 @@ def smtp_client(port=1025, mailserver='127.0.0.1'):
         clientSocket.close()
         return
     # Fill in end
+
+    # Message ends with a single period, send message end and handle server response.
+    # Fill in start
+    try:
+        clientSocket.send(endmsg.encode())
+    # Fill in end
+    except Exception as e:
+        clientSocket.close()
+        return
         
     # Send QUIT command and handle server response.
     # Fill in start
@@ -96,16 +105,7 @@ def smtp_client(port=1025, mailserver='127.0.0.1'):
     except Exception as e:
         clientSocket.close()
         return
-
-    # Message ends with a single period, send message end and handle server response.
-    # Fill in start
-    try:
-        clientSocket.send(endmsg.encode())
-    # Fill in end
-    except Exception as e:
-        clientSocket.close()
-        return
-
+        
     try:
         clientSocket.close()
     except Exception as e:
